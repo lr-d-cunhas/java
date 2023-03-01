1a607ad6aed2918c672fd450f075fe4d20c7c9aa
Rian Lucas <105382777+lr-d-cunhas@users.noreply.github.com>
2023-03-01 08:23:46 -0300

    Add files via upload

---
+++ Introdução a linguagem jav/Programa1.java
@@ -0,0 +1,32 @@
+package geek;
+
+// Variáveis
+public class Programa1 {
+
+	public static void main(String[] args) {
+		// Declaração de variável
+		int valor;
+		
+		int num1, num2, num3;
+		
+		// Declarando e inicializando algumas variáveis
+		int num4, num5 = 4, num6 = 7, num8;
+		
+		
+		char caractere;
+		float preco;
+		
+		// Declarando
+		int num9;
+		
+		// Declarando e inicializando
+		int num10 = 99;
+		
+		
+		float _valor = 34.2f;  // Por padrão, os valores de ponto flutuante são double no java
+		
+		double _valor2 = 34.2;
+		
+	}
+
+}
