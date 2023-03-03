0bc8d46886f32456ef4eabe615beb56bde3a84f8
Rian Lucas <105382777+lr-d-cunhas@users.noreply.github.com>
2023-03-03 16:42:25 -0300

    Add files via upload

---
+++ Introdução a linguagem jav/Programa05.java
@@ -0,0 +1,20 @@
+package geek;
+
+// Operador ternário
+// Se você precisar criar a função main() // programa java
+// main e no teclado pressionar CTRL + barra de espaço 
+
+// digite sysout + CTRL + barra de espaço = System.out.println();
+
+public class Programa05 {
+	public static void main(String[] args) {
+		int valor = 3, numero;
+		
+		if (valor > 0) {
+			numero = valor;
+		}else {
+			numero = 7;
+		}
+		System.out.println(numero);
+	}
+}
