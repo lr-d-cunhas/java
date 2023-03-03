4615194497d0076aae7f90c8bc1d58f98d2233b5
Rian Lucas <105382777+lr-d-cunhas@users.noreply.github.com>
2023-03-03 16:30:57 -0300

    Add files via upload

---
+++ Introdução a linguagem jav/Programa4.java
@@ -0,0 +1,16 @@
+package geek;
+
+public class Programa4 {
+
+	public static void main(String[] args) {
+		// Declarando e inicializando a variável
+		int numero = 9;
+		
+		if (numero > 5) {
+			System.out.println("Sim, o número " + numero + " é maior que 5");
+		}else {
+			System.out.println("Não, o número " + numero + " não é maior que 5");
+		}
+	}
+
+}
