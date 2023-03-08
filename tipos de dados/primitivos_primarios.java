1400da02f8077f5e8913c87d54989c3bf22741eb
Rian Lucas <105382777+lr-d-cunhas@users.noreply.github.com>
2023-03-08 17:29:51 -0300

    Add files via upload

---
+++ tipos de dados/primitivos_primarios.java
@@ -0,0 +1,26 @@
+package geek;
+
+// Tipos de dados
+//Numéricos (Inteiros e Reais)
+public class primitivos_primarios {
+	public static void main(String[] args) {
+		
+		// Tipos primários/primitivos
+		// Por padrão, os números reais em Java são considerados double
+		float preco1 = 23.4f; // 23.40 
+		
+		// para valores muito extensos
+		double preco2 = 23.4; // 23.43423424324213414123423421234423424
+		
+		// tipos não primários/primitivos
+		Float preco3 = 44.5f;
+		Double preco4 = 44.5;
+		
+		System.out.println("float -> preco1 = " + preco1);
+		System.out.println("double -> preco2 = " + preco2);
+		System.out.println("Float -> preco3 = " + preco3);
+		System.out.println("Double -> preco4 = " + preco4);
+		
+	}
+
+}
