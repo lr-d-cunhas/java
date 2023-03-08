0e0b939bc1098f42f8738d36630028e14f8e5a60
Rian Lucas <105382777+lr-d-cunhas@users.noreply.github.com>
2023-03-08 17:21:25 -0300

    Add files via upload

---
+++ tipos de dados/tipos_numericos_int_real.java
@@ -0,0 +1,22 @@
+package geek;
+
+// Tipos de dados
+// Numéricos (Inteiros e Reais)
+public class tipos_numericos_int_real {
+	public static void main(String[] args) {
+		// Tipos primários/primitivos
+		int num1 = 4; // Inteiro 9999999999999
+		short num2 = 4; // Inteiro (curto / menor / baixo) 99999999
+		
+		// Tipos não primários/primitivos
+		Integer num3 = 98;
+		Short num4 = 7;
+		
+		System.out.println("int -> Num1 = " + num1);
+		System.out.println("short -> Num2 = " + num2);
+		System.out.println("Integer -> Num3 = " + num3);
+		System.out.println("Short -> Num4 = " + num4);
+		
+	}
+
+}
