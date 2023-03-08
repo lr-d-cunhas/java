6e40503f6d01714460a5bdf6549bb78095e4af34
Rian Lucas <105382777+lr-d-cunhas@users.noreply.github.com>
2023-03-08 16:35:41 -0300

    Add files via upload

--- Introdução a linguagem jav/_for.java
+++ Introdução a linguagem jav/_for.java
@@ -1,5 +1,29 @@
 package geek;
+import java.util.Scanner;
 
+// for
 public class _for {
-
+	public static void main(String[] args) {
+		int idade;
+		String nome;
+		
+		// Para receber dados do usuário via teclado
+		Scanner teclado = new Scanner(System.in);
+		
+		// variável de controle; condição de parada; forma de incremento
+		// i++ -> i = i + 1
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Informe seu nome: ");
+			nome = teclado.nextLine();
+			
+			System.out.println("Informa sua idade: ");
+			// Idade = teclado.nexInt(); // Bug
+			idade = Integer.parseInt(teclado.nextLine());
+			
+			if (idade > 0) {
+				System.out.println(nome + " tem " + idade + " anos");
+			}
+		}
+		teclado.close();
+	}
 }
